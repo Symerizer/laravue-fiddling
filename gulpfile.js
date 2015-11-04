1c700651bc3286767539720a4ceb296d182217d9
Simon Mercier <contact@symerweb.com>
2015-11-04 00:21:11 -0500

    Added vuejs

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,7 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss');
 });
+
+elixir(function(mix){
+   mix.scripts('../../../bower_components/vue/dist/vue.js');
+});
